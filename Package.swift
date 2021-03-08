005096cd14072d136458fdf76d3afb5b3dd8e0a0
Vignesh J <jvigneshcs@gmail.com>
2021-03-08 18:59:41 +0530

    Made the package to hold multiple targets

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftExtension",
-            targets: ["SwiftExtension"]),
+            name: "SwiftFoundationExtension",
+            targets: ["SwiftFoundationExtension"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftExtension",
-            dependencies: []),
+            name: "SwiftFoundationExtension",
+            dependencies: [],
+            path: "Sources/SwiftExtension/Foundation"),
         .testTarget(
             name: "SwiftExtensionTests",
-            dependencies: ["SwiftExtension"]),
+            dependencies: ["SwiftFoundationExtension"],
+            path: "Tests/SwiftExtensionTests/Foundation"),
     ]
 )
