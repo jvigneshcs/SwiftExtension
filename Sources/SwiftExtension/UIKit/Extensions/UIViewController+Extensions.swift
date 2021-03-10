c8b34d8e622c680ed2829c88a17a1d0e72a4b404
Vignesh J <jvigneshcs@gmail.com>
2021-03-10 13:41:47 +0530

    Added in app local notification support

--- Sources/SwiftExtension/UIKit/Extensions/UIViewController+Extensions.swift
+++ Sources/SwiftExtension/UIKit/Extensions/UIViewController+Extensions.swift
@@ -40,4 +40,108 @@ public extension UIViewController {
                      animated: animated,
                      completion: completion)
     }
+    
+    func addNotifiction(identifier: String,
+                        title: String? = nil,
+                        subtitle: String? = nil,
+                        body: String,
+                        badge: NSNumber? = nil,
+                        launchImageName: String? = nil,
+                        categoryIdentifier: String? = nil,
+                        threadIdentifier: String? = nil) {
+        if #available(iOS 10.0, *) {
+            self.addNotifiction(requestOptions: [.alert, .badge, .sound],
+                                identifier: identifier,
+                                title: title,
+                                subtitle: subtitle,
+                                body: body,
+                                badge: badge,
+                                launchImageName: launchImageName,
+                                categoryIdentifier: categoryIdentifier,
+                                threadIdentifier: threadIdentifier)
+        } else {
+            self.presentAlert(title: title ?? subtitle,
+                              message: body)
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    func addNotifiction(requestOptions options: UNAuthorizationOptions,
+                        identifier: String,
+                        title: String?,
+                        subtitle: String?,
+                        body: String,
+                        badge: NSNumber?,
+                        sound: UNNotificationSound? = nil,
+                        launchImageName: String?,
+                        attachments: [UNNotificationAttachment]? = nil,
+                        categoryIdentifier: String?,
+                        threadIdentifier: String?) {
+        
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: options) { [weak self] (granted, error) in
+                if granted {
+                    self?.addNotificationRequest(identifier: identifier,
+                                                 title: title,
+                                                 subtitle: subtitle,
+                                                 body: body,
+                                                 badge: badge,
+                                                 sound: sound,
+                                                 launchImageName: launchImageName,
+                                                 attachments: attachments,
+                                                 categoryIdentifier: categoryIdentifier,
+                                                 threadIdentifier: threadIdentifier)
+                } else {
+                    self?.presentAlert(title: title ?? subtitle,
+                                      message: body)
+                }
+            }
+    }
+    
+    @available(iOS 10.0, *)
+    private func addNotificationRequest(identifier: String,
+                                        title: String?,
+                                        subtitle: String?,
+                                        body: String,
+                                        badge: NSNumber?,
+                                        sound: UNNotificationSound?,
+                                        launchImageName: String?,
+                                        attachments: [UNNotificationAttachment]?,
+                                        categoryIdentifier: String?,
+                                        threadIdentifier: String?) {
+        let content = UNMutableNotificationContent()
+        
+        if let title = title {
+            content.title = title
+        }
+        if let subTitle = subtitle {
+            content.subtitle = subTitle
+        }
+        if let imageName = launchImageName {
+            content.launchImageName = imageName
+        }
+        if let attachments = attachments {
+            content.attachments = attachments
+        }
+        if let identifier = categoryIdentifier {
+            content.categoryIdentifier = identifier
+        }
+        if let identifier = threadIdentifier {
+            content.threadIdentifier = identifier
+        }
+        content.body = body
+        content.badge = badge
+        content.sound = sound
+        
+        let request = UNNotificationRequest(identifier: identifier,
+                                            content: content,
+                                            trigger: nil)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            guard let error = error else {
+                return
+            }
+            print("Error - \(error.localizedDescription)")
+        }
+        
+    }
 }
